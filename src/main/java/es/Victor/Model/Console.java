a1c6efa0c3ceab7e6fa675ee83ace91b4ff6bf8d
victor <vicheriri@gmail.com>
2018-02-22 19:29:00 +0100

    Console identado y terminado Signed-off-by: victor <vicheriri@gmail.com>

--- src/main/java/es/Victor/Model/Console.java
+++ src/main/java/es/Victor/Model/Console.java
@@ -1,19 +1,22 @@
 package es.Victor.Model;
 
 public class Console {
-	
+
 	private String name;
 	private Integer companyId;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getCompanyId() {
 		return companyId;
 	}
+
 	public void setCompanyId(Integer companyId) {
 		this.companyId = companyId;
 	}
