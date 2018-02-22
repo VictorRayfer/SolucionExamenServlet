529e38a4d57b3383dea1608e4536f8661327eca7
victor <vicheriri@gmail.com>
2018-02-22 13:12:32 +0100

    Models Finished Signed-off-by: victor <vicheriri@gmail.com>

--- src/main/java/es/Victor/Model/Company.java
+++ src/main/java/es/Victor/Model/Company.java
@@ -1,29 +1,48 @@
 package es.Victor.Model;
 
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Company {
-	
+
 	private Integer id;
 	private String name;
 	private Date creationDate;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Date getCreationDate() {
 		return creationDate;
 	}
+
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
+	}
+
+	public void setCreationDateFromString(String creationDate2) {
+		SimpleDateFormat textFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = textFormat.parse(creationDate2);
+		} catch (ParseException ex) {
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+		this.creationDate = date;
 	}
 }
--- src/main/java/es/Victor/Model/Videogame.java
+++ src/main/java/es/Victor/Model/Videogame.java
@@ -1,29 +1,49 @@
 package es.Victor.Model;
 
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Videogame {
-	
+
 	private String title;
 	private Integer pegi;
 	private Date releaseDate;
-	
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public Integer getPegi() {
 		return pegi;
 	}
+
 	public void setPegi(Integer pegi) {
 		this.pegi = pegi;
 	}
+
 	public Date getReleaseDate() {
 		return releaseDate;
 	}
+
 	public void setReleaseDate(Date releaseDate) {
 		this.releaseDate = releaseDate;
 	}
+
+	public void setReleaseDateFromString(String releaseDate2) {
+		SimpleDateFormat textFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = textFormat.parse(releaseDate2);
+		} catch (ParseException ex) {
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+		this.releaseDate = date;
+	}
+
 }
