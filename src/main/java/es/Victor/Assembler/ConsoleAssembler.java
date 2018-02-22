74c3fb43adb3d666b30fe60f4bbfb7fa55563372
victor <vicheriri@gmail.com>
2018-02-22 19:28:24 +0100

    Assemblers identados y terminados Signed-off-by: victor <vicheriri@gmail.com>

--- src/main/java/es/Victor/Assembler/ConsoleAssembler.java
+++ src/main/java/es/Victor/Assembler/ConsoleAssembler.java
@@ -7,7 +7,7 @@ import es.Victor.Model.Console;
 public class ConsoleAssembler {
 
 	public static Console assembleConsoleFrom(HttpServletRequest request) {
-		
+
 		Console console = new Console();
 		console.setName(request.getParameter("name"));
 		console.setCompanyId(Integer.parseInt(request.getParameter("company")));
