31d75ff46456726ab69b8f0540c7c594f8d05db5
victor <vicheriri@gmail.com>
2018-02-22 19:14:56 +0100

    Console Service Signed-off-by: victor <vicheriri@gmail.com>

--- src/main/java/es/Victor/Service/ConsoleService.java
+++ src/main/java/es/Victor/Service/ConsoleService.java
@@ -1,40 +1,43 @@
 package es.Victor.Service;
 
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
 import es.Victor.Assembler.ConsoleAssembler;
+import es.Victor.Connection.ConnectionManager;
+import es.Victor.Connection.H2Connection;
 import es.Victor.Model.Console;
 import es.Victor.Repository.ConsoleRepository;
 
 public class ConsoleService {
-
+	ConsoleAssembler assembler = new ConsoleAssembler();
+	ConnectionManager manager = new H2Connection();
 	private ConsoleRepository repository = new ConsoleRepository();
 
-	public void createNewConsoleFromRequest(HttpServletRequest req) {
-		Console console = ConsoleAssembler.assembleConsoleFrom(req);
-		insertOrUpdate(console);
+	public Console assembleUserFromRequest(HttpServletRequest req) {
+		return ConsoleAssembler.assembleConsoleFrom(req);
 	}
 
-	public void insertOrUpdate(Console consoleFrom) {
-		Console consoleInDatabase = repository.search(consoleFrom);
-		if (null == consoleInDatabase) {
-			repository.insert(consoleFrom);
+	public void createNewConsoleFromRequest(Console consoleForm) {
+		Console consoleInDatabase = repository.search(consoleForm);
+		if (consoleInDatabase == null) {
+			repository.insertConsole(consoleForm);
 		} else {
-			repository.update(consoleFrom);
+			repository.update(consoleForm);
 		}
 	}
 
-	public List<Console> listAllConsole(){
+	public List<Console> listAllConsoles() {
 		return repository.searchAll();
-		
 	}
-	
+
+	public List<Console> listOrderByTitle() {
+		return repository.orderByTitle();
+	}
+
 	public void deleteConsole(Console console) {
 		repository.delete(console);
 	}
-	
+
 	public ConsoleRepository getRepository() {
 		return repository;
 	}
@@ -43,4 +46,7 @@ public class ConsoleService {
 		this.repository = repository;
 	}
 
+	public List<Console> listAllByCompany(int companyId) {
+		return repository.selectByCompany(companyId);
+	}
 }
