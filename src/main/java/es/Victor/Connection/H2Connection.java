66c97a4255e769edb32478d1ff2a4277e16abf70
victor <vicheriri@gmail.com>
2018-02-22 13:18:05 +0100

    Connection modificada y terminada Signed-off-by: victor <vicheriri@gmail.com>

--- src/main/java/es/Victor/Connection/H2Connection.java
+++ src/main/java/es/Victor/Connection/H2Connection.java
@@ -1,19 +1,61 @@
 package es.Victor.Connection;
 
-public class H2Connection extends AbstractConnection {
-	
-	@Override
-	public String getDriver() {
-		return "org.h2.Driver";
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class H2Connection implements ConnectionManager {
+
+	public Connection open(String jdbcUrl) {
+		Connection conn = null;
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection(jdbcUrl + ";INIT=RUNSCRIPT FROM 'classpath:scripts/create.sql'", "sa","");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return conn;
 	}
 
-	@Override
-	public String getDatabaseUser() {
-		return "sa";
+	public Connection executeSql(Connection conn, String sql) {
+		PreparedStatement prepareStatement;
+		try {
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return conn;
 	}
 
-	@Override
-	public String getDatabasePassword() {
-		return "";
+	public void close(Connection conn) {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void close(PreparedStatement prepareStatement) {
+		try {
+			prepareStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void close(ResultSet resultSet) {
+		try {
+			resultSet.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 }
