078ef6765b748cc2a18ae4cc841e3ce90215e393
victor <vicheriri@gmail.com>
2018-02-26 19:34:15 +0100

    FINISH CONNECTION AFTER PULLREQUEST Signed-off-by: victor <vicheriri@gmail.com>

--- src/main/java/es/Victor/Connection/H2Connection.java
+++ src/main/java/es/Victor/Connection/H2Connection.java
@@ -1,11 +1,5 @@
 package es.Victor.Connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class H2Connection extends AbstractConnection {
 
 	@Override
